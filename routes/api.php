fb8d0a285c30d8be33f9634eb017c315a064a02c
fahad imran <fahadimran020@gmail.com>
2021-12-20 16:08:35 +0500

    UserRegistrationController

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('food_menu_category',[App\Http\Controllers\FoodMenuController::class,'FoodCategory']);
 Route::post('user_login',[App\Http\Controllers\UserLoginController::class,'UserLogin']);
+Route::post('user_registration',[App\Http\Controllers\UserRegistrationController::class,'UserRegistration']);
 Route::get('food_category',[App\Http\Controllers\FoodCategoryController::class,'FoodCategory']);
 Route::get('food_order',[App\Http\Controllers\OrderController::class,'Order']);
 Route::post('create_order',[App\Http\Controllers\OrderController::class,'StorOrder']);
